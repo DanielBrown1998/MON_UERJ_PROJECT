ff3c7f340370b39cdc46b47b931dec0d6b8865d7
Daniel <daniel_mingozzi@hotmail.com>
2024-07-18 11:15:48 -0300

    refactor: modificando a base de dados

--- home/admin.py
+++ home/admin.py
@@ -6,36 +6,19 @@ from home import models
 
 @admin.register(models.Monitorias)
 class MonitoriasAdmin(admin.ModelAdmin):
-    list_display = 'date', 'matricula',
-    list_filter = 'date', 'matricula',
-    search_fields = 'date', 'matricula',
+    list_display = 'date', 'username',
+    list_filter = 'date', 'username',
+    search_fields = 'date', 'username',
     list_per_page = 10
     list_max_show_all = 100
 
 
-@admin.register(models.Matricula)
-class MatriculaAdmin(admin.ModelAdmin):
-    list_display = 'matricula', 
-    list_filter = 'matricula',
-    search_fields = 'matricula',
-    list_per_page = 10
-    list_max_show_all = 60
-
-
 @admin.register(models.DataUser)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'matricula',
-        'name', 
-        'email', 
-        'phone',  
-        'monitorias_marcadas', 
-        'monitorias_presentes',
-        )
+class DataUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'monitorias_marcadas', 'monitorias_presentes',)
     
-    ordering = 'matricula', 
     list_filter = 'monitorias_presentes',
-    search_fields = 'matricula', 'name', 'email', 'phone', 'monitorias',
+    search_fields = 'username', 'first_name', 'email',
     list_per_page = 10
     list_max_show_all = 60
-    list_editable = 'monitorias_presentes',
+    list_editable = 'monitorias_presentes', 'is_staff', 'email',
--- home/models.py
+++ home/models.py
@@ -1,43 +1,28 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-
-DEFAULT = "000000000000"
-PK = True
-
-class Matricula(models.Model):
-    
-    class Meta:
-        verbose_name = 'Matricula'
-        verbose_name_plural = 'Matriculas'
-    
-    matricula = models.CharField(max_length=12, primary_key=PK, default= DEFAULT)
-    staff = models.BooleanField(default=False)
-
-
-class DataUser(models.Model):
+class DataUser(User):
 
     class Meta:
-        verbose_name = 'DataUser'
-        verbose_name_plural = 'DataUsers'
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
 
-    matricula = models.ForeignKey(Matricula, on_delete=models.CASCADE, primary_key=True)
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=9)
     monitorias_marcadas = models.IntegerField(default=0)        
     monitorias_presentes = models.IntegerField(default=0)
     
     def __str__(self):
-        return self.name
+        return self.owner.first_name + self.owner.last_name
 
 
 class Monitorias(models.Model):
     
+    date = models.DateField()
+    username = models.ForeignKey(DataUser, on_delete=models.DO_NOTHING)
+
     class Meta:
         verbose_name_plural = "Monitorias"
-        unique_together = ('date', 'matricula'),
+        unique_together = ('date', 'username'),
 
-    date = models.DateField()
-    matricula = models.ForeignKey(Matricula, on_delete=models.DO_NOTHING)
+ 
