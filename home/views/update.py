7ff609a9025d310e13010a0c8c00ea8dc28548c9
Daniel <daniel_academico1998@hotmail.com>
2024-08-16 09:39:31 -0300

    wip: tronando os forms de update mais dinâmicos

--- home/views/update.py
+++ home/views/update.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
+from home.forms.updateform import UpdateForm
 from home.models import User
 from datetime import datetime
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django import forms
+from django.core.exceptions import ValidationError
 
 
 def update(request):
     
+    if request.method == 'POST':
+        print(request.POST)
+        context = {
+        'title': 'Update',
+        'form': UpdateForm(
+            request.POST
+            ),
+        }
+        url = 'home/update.html'
+        return render(request, url, context=context)
+        
+
+
+
+
+
     context = {
         'title': 'Update',
+        'form': UpdateForm(),
     }
     url = 'home/update.html'
     return render(request, url, context=context)
